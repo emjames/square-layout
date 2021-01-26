1db9574129ca2b0fa44d7ebcb416ee543c782629
ej <ej@emjames.com>
2021-01-26 15:38:14 +0800

    Use textarea instead of input text, add styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// Input example
+// type,name,row,column
+// 7,staggered square pin fin,0,0
+// 7,staggered square pin fin,0,1
+// 6,in-line square pin fin,0,2
+// 4,in-line circular pin fin,1,0
+// 4,in-line circular pin fin,1,1
+// 0,none,1,2
+
 let main = null;
 
 function showErrorMessage() {
@@ -13,7 +22,9 @@ function clearErrorMessage() {
 
 function makeFigure() {
     const layoutText = document.getElementById("layout").value;
-    const layout = layoutText.split(",").map(num => parseInt(num));
+    ///const layout = layoutText.split(",").map(num => parseInt(num));
+
+    layout = parseLayout(layoutText);
 
     if (layout.length === 1 && isNaN(layout[0])) {
         showErrorMessage();
@@ -21,6 +32,49 @@ function makeFigure() {
         clearErrorMessage();
         createLayout(layout);
     }
+}
+
+function parseLayout(layout) {
+    // Minimal specification length (e.g. [0, none, 1, 2])
+    const WHITE_SPACE_LEN = 1;
+    // All lines from the textarea
+    const allLines = layout.split("\n");
+    // First line containing the line keys
+    const lineKeys = allLines[0].split(',');
+    // Specification lines
+    const specLines = allLines.slice(1);
+    // Index of elements that should be ints
+    const intIndex = [0, 2, 3];
+
+    // Array of objects that holds each line made into an object
+    let parsedLines = [];
+
+    // Make objects from each line
+    for (let i = 0; i < specLines.length; i++) {
+        // Each line should be a csv
+        const specs = specLines[i].split(',');
+
+        // Check that the line is not empty nor has extra empty space
+        if (specs.length != WHITE_SPACE_LEN) {
+            // The object that will be appended to parsedLines
+            const obj = {}
+
+            // Loop through each spec and assign the key to value
+            for (let j=0; j < specs.length; j++) {
+                let value = specs[j];
+
+                // Check if we need to convert the value to int
+                if (intIndex.includes(j)) {
+                    value = parseInt(value);
+                }
+
+                obj[lineKeys[j]] = value;
+            }
+            parsedLines.push(obj);
+        }
+    }
+
+    return parsedLines;
 }
 
 function createSquare(text, width, height) {
@@ -43,22 +97,29 @@ function clearMainSquare() {
 function createLayout(layout) {
     clearMainSquare();
 
-    rows = layout.length;
-    for (let i = rows; i > 0; i--) {
-        rowElements = layout[i - 1];
-        for (let j = 0; j < rowElements; j++) {
-          // The content in the square (e.g. (3, 1) )
-          squareText = "(" + i + "," + (j + 1) + ")";
+    // Find the max row in the array of objects (add once since we index from 0)
+    const maxRow = layout.reduce((prev, curr) => {
+        return (prev.row > curr.row) ? prev : curr
+    }).row + 1;
 
-          // Calculate the percentage for the width depending on the rowElements
-          squareWidth = 100 / rowElements - 10 + "%";
+    // Find the max col in the array of objects (add once since we index from 0)
+    const maxCol = layout.reduce((prev, curr) => {
+        return (prev.column > curr.column) ? prev : curr
+    }).column + 1;
 
-          // Calculate the percentage for the height depending on the row(s) number
-          squareHeight = 100 / rows - 10 + "%";
 
-          newSquare = createSquare(squareText, squareWidth, squareHeight);
+    for (let i = 1; i <= maxRow; i++) {
+        for (let j = 1; j <= maxCol; j++) {
+            // The content in the square (e.g. (3, 1) )
+            squareText = "(" + i + "," + j + ")";
 
-          main.appendChild(newSquare);
+            // Calculate the percentage for the width depending on the columns number
+            squareWidth = 100 / maxCol - 10 + "%";
+            // Calculate the percentage for the height depending on the row(s) number
+            squareHeight = 100 / maxRow - 10 + "%";
+
+            newSquare = createSquare(squareText, squareWidth, squareHeight);
+            main.appendChild(newSquare);
         }
     }
 }
